83eb63ed6842ef58cb4fc30bf52c63c80dbea751
nomanoma121 <takeuchi.k.747@ms.saitama-u.ac.jp>
2025-04-17 13:29:56 +0900

    feat: ipを固定

--- server/server-1/main.go
+++ server/server-1/main.go
@@ -6,6 +6,16 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	// リクエストヘッダーをlogに出力
+	fmt.Println("Request Headers:")
+
+	for name, values := range r.Header {
+		// Loop over all values for the name.
+		for _, value := range values {
+			fmt.Printf("%s: %s\n", name, value)
+		}
+	}
+	
 	fmt.Fprintf(w, "Hello from server-1!\n")
 }
 
